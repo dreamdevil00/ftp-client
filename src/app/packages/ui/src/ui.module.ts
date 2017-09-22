32de561561395030ea67b42b69cb24d3902901b5
roadtodream <dreamdeviloo@163.com>
2017-09-22 17:48:08 +0800

    add alert to ui

--- src/app/packages/ui/src/ui.module.ts
+++ src/app/packages/ui/src/ui.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { UiLogoComponent } from './components/logo/logo.component'
 import { UiMessageComponent } from './components/message/message.component'
+
+import { UiService } from './services/ui.service'
 
 const components = [
   UiLogoComponent,
@@ -13,6 +16,11 @@ const components = [
 const modules = [
   CommonModule,
   FormsModule,
+  NgbModule.forRoot(),
+]
+
+const providers = [
+  UiService,
 ]
 
 @NgModule({
@@ -24,6 +32,9 @@ const modules = [
   ],
   exports: [
     ...components,
+  ],
+  providers: [
+    ...providers,
   ]
 })
 export class DreamUiModule {}
