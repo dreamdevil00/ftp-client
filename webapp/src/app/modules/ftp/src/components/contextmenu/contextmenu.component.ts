01bb4c3ac12c719cb57e86e321cff62b491fe0c0
roadtodream <dreamdeviloo@163.com>
2017-09-22 14:57:48 +0800

    add ftp module components

---
+++ webapp/src/app/modules/ftp/src/components/contextmenu/contextmenu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: './contextmenu.component.html'
+})
+export class ContextMenuComponent {
+
+}
---
+++ webapp/src/app/modules/ftp/src/components/transfer-window/transfer-window.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: './transfer-window.component.html'
+})
+export class TransferWindowComponent {}
