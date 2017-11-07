392911e093c3f041ee3ae5e09e2ebf9984f55fd8
roadtodream <dreamdeviloo@163.com>
2017-11-07 13:46:33 +0800

    change renderer out dir

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = {
     ]
   },
   "output": {
-    "path": path.join(process.cwd(), "dist"),
+    "path": path.join(process.cwd(), "dist/renderer"),
     "filename": "[name].bundle.js",
     "chunkFilename": "[id].chunk.js"
   },
